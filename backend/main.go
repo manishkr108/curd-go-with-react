d0a2906fa8808a01a825e4d8374dbb18fa834534
manish kumar <manish@landsandhomes.com>
2024-09-14 21:02:48 +0530

    CROSS error solve

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"backend/db"
 	"backend/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,6 +15,13 @@ func main() {
 	}
 	db.CreateTable() // Ensure table creati
 	server := gin.Default()
+
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"http://localhost:3000"}
+	corsConfig.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	corsConfig.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
+
+	server.Use(cors.New(corsConfig))
 
 	routes.RegisterRoutes(server)
 	server.Run(":8080") // localhost:8080
